a66dd3fbfc07eef509152efdb8b0eb70ca6741ad
Karim Omar <okareem250@gmail.com>
2023-10-02 15:29:37 +0200

    creat book view

---
+++ lib/Features/home/views/book_view.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/Features/home/views/widgets/book_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookView extends StatelessWidget {
+  const BookView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+body: BookViewBody(),
+
+    );
+  }
+}
