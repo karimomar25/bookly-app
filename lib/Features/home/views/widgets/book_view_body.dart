23cf809cfba266d2f95e7036a41a47b019d82f9b
Karim Omar <okareem250@gmail.com>
2023-10-05 12:32:24 +0200

    add title and sub to book view

--- lib/Features/home/views/widgets/book_view_body.dart
+++ lib/Features/home/views/widgets/book_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/home/views/widgets/custom_appbar_bookview.dart';
 import 'package:bookly_app/Features/home/views/widgets/featured_book.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookViewBody extends StatelessWidget {
@@ -14,9 +15,27 @@ class BookViewBody extends StatelessWidget {
         children: [
           const AppBarBookView(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17 ),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const FeatueredBook(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            "The Jungle Book",
+            style: Styles.TextStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "Rudyard Kipling",
+              style: Styles.TextStyle18.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          )
         ],
       ),
     );
