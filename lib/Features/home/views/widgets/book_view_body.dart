cf2ad5fd9162be4924819dd6e921821092a3f4bd
Karim Omar <okareem250@gmail.com>
2023-10-07 14:45:38 +0200

    add custom scroll view

--- lib/Features/home/views/widgets/book_view_body.dart
+++ lib/Features/home/views/widgets/book_view_body.dart
@@ -12,63 +12,73 @@ class BookViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const AppBarBookView(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: const FeatueredBook(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          const Text(
-            "The Jungle Book",
-            style: Styles.TextStyle30,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              "Rudyard Kipling",
-              style: Styles.TextStyle18.copyWith(
-                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const AppBarBookView(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .2),
+                  child: const FeatueredBook(),
+                ),
+                const SizedBox(
+                  height: 43,
+                ),
+                const Text(
+                  "The Jungle Book",
+                  style: Styles.TextStyle30,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    "Rudyard Kipling",
+                    style: Styles.TextStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 37,
+                ),
+                const BooksAction(),
+                Expanded(
+                  child: const SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "You can also like",
+                    style: Styles.TextStyle14.copyWith(
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SimilarBooksListView(),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
             ),
           ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BooksAction(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "You can also like",
-              style: Styles.TextStyle14.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          SimilarBooksListView(),
-          const SizedBox(
-            height: 40,
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
-
